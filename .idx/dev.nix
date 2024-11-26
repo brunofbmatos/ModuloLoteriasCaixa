0fb9905e81c1f468f22a3bf5c92adf060e2fa230
bruno.f.b.matos <bruno.f.b.matos@gmail.com>
2024-11-26 01:57:38 +0000

    alteração .nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,59 +1,16 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs ? import <nixpkgs> {} }: {
-  pkgs.mkShell {
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
   buildInputs = with pkgs; [
-    # Seus pacotes aqui
-    powershell
+    # Ferramentas de desenvolvimento
+    #nodeJs
+    #yarn
+    # Editores de texto
+    vim
+    # PowerShell e suas dependências (ajuste conforme necessário)
+    powershell_7.4  
+    #dotnet5
   ];
-  channel = "stable-24.05"; # or "unstable"
-
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
-  ];
-
-  # Sets environment variables in the workspace
-  env = {};
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      # "vscodevim.vim"
-    ];
-
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
-    };
-  };
+  shellHook = ''
+    # Scripts a serem executados ao iniciar o shell
+  '';
 }
